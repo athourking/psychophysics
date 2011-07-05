5cb2114b8340c7c1b981df09b688d895ea01b7a6
icelandhouse <icelandhouse@gmail.com>
2011-07-05 21:16:48 +0200

    Just clearing up conflicts

--- analyze_sub.m
+++ analyze_sub.m
@@ -21,8 +21,6 @@ accuracy_sem = std(accuracy,0, 3) ./ sqrt(size(accuracy, 3));
 proportion_sem = std(accuracy,0, 3) ./ sqrt(size(accuracy, 3));
 meanProportion_sem_half = proportion_sem / 2;
 
-titles = {'Backward masking', 'Forward masking', 'Middle masking', 'Control condition'};
-
 figure()
 hold on
 line([0 1],[0.25 1]); % linear trend expected    
@@ -48,7 +46,7 @@ for maskCond = 1:size(meanAccuracy,1)
     end
 end
 
-title('');
+title('Accuracy as function of proportion of "yes" responses','fontsize',14);
 xlabel('Proportion of "yes" responses');
 ylabel('Accuracy');
 set(gca,'Xlim', [0 1])
