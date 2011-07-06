e8cecebf7a86660d6dfde99f800ae2cf82433604
Lisandro Kaunitz <lisandro.kaunitz@unitn.it>
2011-07-06 16:09:58 +0200

    Put directories in .gitignore -now it'll be local- and added directories_template

---
+++ directories_template.m
@@ -0,0 +1,10 @@
+function [rawDataDir resultsDir figsDir fileName] = directories_template
+
+rawDataDir = '';
+resultsDir = '';
+figsDir = '';
+
+fileName = 'Data_12Subjects'; % DEFINE THE NAME TO SAVE THE FILE, contains a matrix of all subjects' trials
+
+
+
